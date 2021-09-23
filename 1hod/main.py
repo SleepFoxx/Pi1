536faf583f9e1a3a3d7b3053c3bef5301a874588
z21ite08 <z21ite08@z00p3-07.SPSKNM>
2021-09-23 14:43:49 +0200

    1 hodina

---
+++ 1hod/main.py
@@ -0,0 +1,18 @@
+# toto je komentár
+
+print ("Hello world")
+x = 10
+y = 20
+obvod = (x+y) * 2
+obsah = x * y
+print("Obvod obdlznika je", obvod)
+print("Obsah obdlznika je", obsah)
+
+tmp = x
+x = y
+y = tmp
+print(x)
+
+
+
+
