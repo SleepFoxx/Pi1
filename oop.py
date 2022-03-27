a6355a79c47a2bb579a067009a808f6c02599854
Mr.Papa <jakubhulin30@gmail.com>
2022-03-27 11:35:09 +0200

    Cvicenia na OOP

--- oop.py
+++ oop.py
@@ -86,7 +86,7 @@ print(f"{a} + {b} = {calc.add()}")
 print(f"{a} - {b} = {calc.sub()}")
 print(f"{a} * {b} = {calc.mul()}")
 print(f"{a} / {b} = {calc.div()}")
-"""
+
 import random
 
 hrany = int(input("Zadaj pocet hran: "))
@@ -105,10 +105,116 @@ for i in range(0, 10):
     print(dice.hodkockou())
 
 
-    
-    
+class Car:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.milaage = mileage
+
+car = Car(230, 10000)
+print(car.max_speed, car.milaage)
 
 
+class Car:
+    pass
+
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.milaage = mileage
+
+class Bus(Vehicle):
+    pass
+
+school_bus = Bus("School Volvo", 180, 12)
+print("Vehicle name: ", school_bus.name, "Speed: ", school_bus.max_speed, "Mileage: ", school_bus.milaage)
+
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+
+bus = Bus("School Volvo", 180, 12)
+print(bus.seating_capacity())
+
+
+class Vehicle:
+    color = "White"
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+    
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+bus = Bus("School Volvo", 180, 12)
+car = Car("Volvo", 180, 12)
+print("Color: ", bus.color, "Vehicle name:", bus.name, "Speed: ", bus.max_speed, "Mileage: ", bus.mileage)
+print("Color: ", car.color, "Vehicle name:", car.name, "Speed: ", car.max_speed, "Mileage: ", car.mileage)
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10 / 100
+        return amount
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+class Bus(Vehicle):
+    pass
+
+school_bus = Bus("School Volvo", 12, 50)
+
+print(type(school_bus))
+
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+print(isinstance(School_bus, Vehicle))
+"""
 
 
 
